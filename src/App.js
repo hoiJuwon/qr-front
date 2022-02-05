22971f2fae09681b82eaa5eaeb92f28220be98f5
hoiJuwon <withjuwon@gmail.com>
2022-02-05 14:05:07 +0900

    [fix] package.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
 
 function App() {
-  return <div>hello</div>;
+  const [inputs, setInputs] = useState({
+    userName: "",
+  });
+
+  const { name } = inputs;
+
+  const registerUserName = (userName) => {
+    window.localStorage.setItem("userName", userName);
+  };
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const generateQrHandler = () => {
+    // TODO : generateQr with userName
+  };
+
+  return (
+    <div>
+      <input placeholder="name" onChange={onChange} value={name} />
+      <button type="submit" onClick={generateQrHandler}>
+        generate qr
+      </button>
+      <QRCode value="https://www.naver.com" />
+    </div>
+  );
 }
 
 export default App;
