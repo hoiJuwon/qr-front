6cdbcab793f9286f04868bce670aa0f0d901ab1e
hoiJuwon <withjuwon@gmail.com>
2022-02-08 13:22:02 +0900

    [feat] 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import React, { useState } from "react";
-import QRCode from "react-qr-code";
+import { Participant, Admin } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [inputs, setInputs] = useState({
-    userName: "",
-  });
-
-  const { name } = inputs;
-
-  const registerUserName = (userName) => {
-    window.localStorage.setItem("userName", userName);
-  };
-
-  const onChange = (e) => {
-    const { value, name } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
-
-  const generateQrHandler = () => {
-    // TODO : generateQr with userName
-  };
-
   return (
-    <div>
-      <input placeholder="name" onChange={onChange} value={name} />
-      <button type="submit" onClick={generateQrHandler}>
-        generate qr
-      </button>
-      <QRCode value="https://www.naver.com" />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Participant />} />
+        <Route path="admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
